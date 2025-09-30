fe74f6c4536dde6cf7cae1cf485563e9992f5349
Isabel Figueroa <isabel.figueroa@ucr.ac.cr>
2025-09-29 22:44:37 -0600

    Deployed 31388b5c with MkDocs version: 1.6.1

---
+++ javascripts/mathjax.js
@@ -0,0 +1,5 @@
+// Configuración mínima de MathJax para arithmatex
+window.MathJax = {
+  tex: { inlineMath: [['$', '$'], ['\\(', '\\)']] },
+  options: { skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre'] }
+};
